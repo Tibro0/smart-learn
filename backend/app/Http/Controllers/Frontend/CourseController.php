fbdcd16cdd6c990051452f5be4c8d8d80f788b38
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-06-01 23:57:05 +0600

    Create Course Api

---
+++ backend/app/Http/Controllers/Frontend/CourseController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'title' => 'required|min:5'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ],400);
+        }
+
+        // This will store course in db
+        $course = new Course();
+        $course->title = $request->title;
+        $course->status = 0;
+        $course->user_id = $request->user()->id;
+        $course->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Course Created Successfully!'
+        ],200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- backend/database/migrations/2025_06_01_080035_create_courses_table.php
+++ backend/database/migrations/2025_06_01_080035_create_courses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->unique();
+            $table->string('title');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('category_id')->nullable()->constrained()->onDelete('cascade');
             $table->foreignId('level_id')->nullable()->constrained()->onDelete('cascade');
