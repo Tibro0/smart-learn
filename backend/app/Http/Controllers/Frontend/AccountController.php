9a6d447e726101d852dbd41ffc6cffb90ab80e0e
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-06-01 19:12:05 +0600

    Laravel Authenticate User

--- backend/app/Http/Controllers/Frontend/AccountController.php
+++ backend/app/Http/Controllers/Frontend/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -36,5 +37,37 @@ class AccountController extends Controller
             'status' => 200,
             'message' => 'User Registered Successfully!'
         ],200);
+    }
+
+    public function authenticate(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // this will return validator errors
+        if ($validator->fails()) {
+            return response()->json([
+            'status' => 400,
+            'errors' => $validator->errors()
+            ],400);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = User::findOrFail(Auth::user()->id);
+            $token = $user->createToken('token')->plainTextToken;
+
+            return response()->json([
+                'status' => 200,
+                'token' => $token,
+                'name' => $user->name,
+                'id' => Auth::user()->id,
+            ],200);
+        }else {
+            return response()->json([
+                'status' => 401,
+                'message' => 'Either Email or Password is Incorrect!'
+            ],401);
+        }
     }
 }
