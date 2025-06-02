32dc48dca65736ee59b66787407d945b7c4ce113
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-06-02 19:03:30 +0600

    Course Outcome API Laravel

---
+++ backend/app/Http/Controllers/Frontend/OutcomeController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Outcome;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class OutcomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $outcomes = Outcome::where('course_id', $request->course_id)->get();
+        return response()->json([
+            'status' => 200,
+            'data' => $outcomes
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'outcome' => 'required',
+            'course_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $outcome = new Outcome();
+        $outcome->course_id     = $request->course_id;
+        $outcome->text          = $request->outcome;
+        $outcome->sort_order    = 1000;
+        $outcome->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Outcomes Added Successfully!'
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $outcome = Outcome::findOrFail($id);
+
+        if ($outcome === null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Outcome Not Found!'
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'outcome' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+
+        $outcome->text = $request->outcome;
+        $outcome->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Outcome Updated Successfully!'
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $outcome = Outcome::findOrFail($id);
+
+        if ($outcome === null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Outcome Not Found!'
+            ], 404);
+        }
+
+        $outcome->delete();
+
+        return response()->json([
+                'status' => 200,
+                'message' => 'Outcome Deleted Successfully!'
+            ], 200);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Frontend\AccountController;
 use App\Http\Controllers\Frontend\CourseController;
+use App\Http\Controllers\Frontend\OutcomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,10 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-
+    // Course All Route
     Route::get('/courses/meta-data', [CourseController::class, 'metaData']);
     Route::resource('/courses', CourseController::class);
+
+    // Outcome All Route
+    Route::resource('/outcomes', OutcomeController::class);
 });
