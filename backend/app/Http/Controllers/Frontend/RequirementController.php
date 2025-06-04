fd2fcbf857fe44f6a7181fdec578d4d8e9b530cd
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-06-05 00:03:45 +0600

    Course Requirement API

---
+++ backend/app/Http/Controllers/Frontend/RequirementController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Requirement;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class RequirementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $requirements = Requirement::where('course_id', $request->course_id)->get();
+        return response()->json([
+            'status' => 200,
+            'data' => $requirements
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'requirement' => 'required',
+            'course_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $requirement = new Requirement();
+        $requirement->course_id     = $request->course_id;
+        $requirement->text          = $request->requirement;
+        $requirement->sort_order    = 1000;
+        $requirement->save();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $requirement,
+            'message' => 'Requirement Added Successfully!'
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $requirement = Requirement::findOrFail($id);
+
+        if ($requirement === null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Requirement Not Found!'
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'requirement' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+
+        $requirement->text = $request->requirement;
+        $requirement->save();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $requirement,
+            'message' => 'Requirement Updated Successfully!'
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $requirement = Requirement::findOrFail($id);
+
+        if ($requirement === null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Requirement Not Found!'
+            ], 404);
+        }
+
+        $requirement->delete();
+
+        return response()->json([
+                'status' => 200,
+                'message' => 'Requirement Deleted Successfully!'
+            ], 200);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Frontend\AccountController;
 use App\Http\Controllers\Frontend\CourseController;
 use App\Http\Controllers\Frontend\OutcomeController;
+use App\Http\Controllers\Frontend\RequirementController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +18,8 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     // Course All Route
     Route::get('/courses/meta-data', [CourseController::class, 'metaData']);
     Route::resource('/courses', CourseController::class);
-
     // Outcome All Route
     Route::resource('/outcomes', OutcomeController::class);
+    // Requirement All Route
+    Route::resource('/requirements', RequirementController::class);
 });
