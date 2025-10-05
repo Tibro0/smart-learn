de8a4909ae3cffb92a8f36b567e85a217f7fa13d
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-06 01:17:59 +0600

    Authenticate API Backend

--- backend/app/Http/Controllers/Frontend/AccountController.php
+++ backend/app/Http/Controllers/Frontend/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -37,5 +38,39 @@ class AccountController extends Controller
             'status' => 200,
             'message' => 'User Registered Successfully!'
         ], 200);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // this will return validator errors
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = User::find(Auth::user()->id);
+
+            $token = $user->createToken('token')->plainTextToken;
+
+            return response()->json([
+                'status' => 200,
+                'token' => $token,
+                'name' => $user->name,
+                'id' => Auth::user()->id,
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 401,
+                'message' => 'Either Email/Password Incorrect.',
+            ], 401);
+        }
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,4 +10,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AccountController::class)->group(function () {
     Route::post('register', 'register');
+    Route::post('login', 'authenticate');
 });
