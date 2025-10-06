a478cc62fa1a4dda3dfad7ab6a04317fd2c498a1
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-06 21:31:49 +0600

    Get All Categories, Levels & Languages

--- backend/app/Http/Controllers/Frontend/CourseController.php
+++ backend/app/Http/Controllers/Frontend/CourseController.php
@@ -3,23 +3,15 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Course;
+use App\Models\Language;
+use App\Models\Level;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class CourseController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -47,35 +39,18 @@ class CourseController extends Controller
         ], 200);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    // This method will return categories/levels/languages
+    public function metaData()
     {
-        //
-    }
+        $categories = Category::all();
+        $levels = Level::all();
+        $languages = Language::all();
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'status' => 200,
+            'categories' => $categories,
+            'levels' => $levels,
+            'languages' => $languages,
+        ], 200);
     }
 }
