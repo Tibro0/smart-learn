7a155804596d484c36e5d8db0abf31a5ad2b68fa
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-12 22:01:14 +0600

    Show Requirements Frontend

--- frontend/src/components/pages/account/courses/ManageRequirement.jsx
+++ frontend/src/components/pages/account/courses/ManageRequirement.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { apiUrl, token } from "../../../common/Config";
+import { MdDragIndicator } from "react-icons/md";
+import { BsPencilSquare } from "react-icons/bs";
+import { FaTrashAlt } from "react-icons/fa";
 
 const ManageRequirement = () => {
   const [loading, setLoading] = useState(false);
@@ -11,12 +14,14 @@ const ManageRequirement = () => {
   const params = useParams();
 
   const {
-      register,
-      handleSubmit,
-      setError,
-      formState: { errors },
-      reset,
-    } = useForm();
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  
 
   const onSubmit = async (data) => {
     setLoading(true);
@@ -48,6 +53,32 @@ const ManageRequirement = () => {
       });
   };
 
+  const fetchRequirements = async () => {
+    await fetch(`${apiUrl}/requirements?course_id=${params.id}`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.status == 200) {
+          setRequirements(result.data);
+        } else {
+          const errors = result.errors;
+          Object.keys(errors).forEach((field) => {
+            setError(field, { message: errors[field][0] });
+          });
+        }
+      });
+  };
+
+  useEffect(() => {
+    fetchRequirements();
+  }, []);
+
   return (
     <>
       <div className="card shadow border-0 mt-4">
@@ -73,6 +104,39 @@ const ManageRequirement = () => {
               {loading == false ? "Save" : "Please Wait.."}
             </button>
           </form>
+
+          {requirements &&
+            requirements.map((requirement) => {
+              return (
+                <div
+                  key={`requirement-${requirement.id}`}
+                  className="card shadow border-0 mb-2"
+                >
+                  <div className="card-body p-2 d-flex">
+                    <div>
+                      <MdDragIndicator />
+                    </div>
+                    <div className="d-flex justify-content-between w-100">
+                      <div className="ps-2">{requirement.text}</div>
+                      <div className="d-flex">
+                        <Link
+                          onClick={() => handleShow(requirement)}
+                          className="text-primary me-1"
+                        >
+                          <BsPencilSquare />
+                        </Link>
+                        <Link
+                          onClick={() => deleteOutcome(requirement.id)}
+                          className="text-danger"
+                        >
+                          <FaTrashAlt />
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
     </>
