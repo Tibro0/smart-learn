33b2031c42d8370a71a674251e38016a14836c37
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-10-14 20:32:23 +0600

    Create Chapter Relation Backend

--- backend/app/Models/Chapter.php
+++ backend/app/Models/Chapter.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Chapter extends Model
 {
     protected $guarded = [];
+
+    public function lessons()
+    {
+        return $this->hasMany(Lesson::class);
+    }
 }
